95da360006ac3417d92144f5e18a29fb8ffc6774
Anatolii Litynskiy <litinskiy.tolik@gmail.com>
2023-04-18 01:30:12 +0300

    some updates

--- src/components/Unknown/CurrencyTab/index.tsx
+++ src/components/Unknown/CurrencyTab/index.tsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-// import { getCurrency } from "../../../services/common/privateBankService";
-// import { PrivateBankCurrency } from "../../../types";
+import { getCurrency } from "../../../services/privateBankService";
+import { PrivateBankCurrency } from "../../../types";
 import "./styles.scss";
 
-import { testData } from "./testData";
+// import { testData } from "./testData";
 
 const CurrencyTab: React.FC = () => {
-  // const [currency, setCurrensy] = useState<PrivateBankCurrency[] | null>(null);
+  const [currency, setCurrensy] = useState<PrivateBankCurrency[] | null>(null);
 
-  // useEffect(() => {
-  //   getCurrency()
-  //     .then((result) => setCurrensy(result))
-  //     .catch((error) => {
-  //       // TODO add notofocation
-  //       console.log(error);
-  //     });
-  // }, []);
+  useEffect(() => {
+    getCurrency()
+      .then((result) => setCurrensy(result))
+      .catch((error) => {
+        // TODO add notofocation
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="currency">
@@ -26,8 +26,8 @@ const CurrencyTab: React.FC = () => {
         <div className="currency__head__title">Sale</div>
       </div>
       <div className="currency__body">
-        {testData ? (
-          testData.map((curr) => (
+        {currency ? (
+          currency.map((curr) => (
             <ul key={curr.ccy} className="currency__body__row">
               <li className="currency__body__item">{curr.ccy}</li>
               <li className="currency__body__item">{curr.buy}</li>
